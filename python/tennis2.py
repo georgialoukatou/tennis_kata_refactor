994d9c5c43c259e67f8c77e1cb228160400527ef
georgialoukatou@gmail.com <georgialoukatou@gmail.com>
2024-10-15 00:09:25 +0200

    refactor(TennisGame2): Add error handling and score constants in score class
    - Introduced variables at the beginning of the class to avoid hardcoded fixed values.
    - Added error case handling players having the same name.

--- python/tennis2.py
+++ python/tennis2.py
@@ -1,7 +1,15 @@
 class TennisGame2:
     SCORE_MAPPING = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
+    DEUCE = "Deuce"
+    SCORE_ADVANTAGE_POSITIVE = 1
+    SCORE_ADVANTAGE_NEGATIVE = -1
+    SCORE_WIN_POSITIVE= 2
+    SCORE_WIN_NEGATIVE = -2
+    SCORE_DEUCE = 0
 
     def __init__(self, player1_name, player2_name):
+        if player1_name == player2_name:
+            raise ValueError("Players have the same name")
         self._player1_name = player1_name
         self._player2_name = player2_name
         self._p1_points = 0
@@ -21,16 +29,18 @@ class TennisGame2:
 
     def _get_advantage_or_win_score(self):
         score_diff = self._calculate_score_difference()
-        if score_diff >= 2:
-            return "Win for player1"
-        if score_diff <= -2:
-            return "Win for player2"
-        if score_diff == 1:
-            return "Advantage player1"
-        if score_diff == -1:
-            return "Advantage player2"
-        if score_diff == 0:
-            return "Deuce"
+        if score_diff == self.SCORE_ADVANTAGE_POSITIVE:
+            return f"Advantage {self._player1_name}"
+        elif score_diff == self.SCORE_ADVANTAGE_NEGATIVE:
+            return f"Advantage {self._player2_name}"
+        elif score_diff >= self.SCORE_WIN_POSITIVE:
+            return f"Win for {self._player1_name}"
+        elif score_diff <= self.SCORE_WIN_NEGATIVE:
+            return f"Win for {self._player2_name}"
+        elif score_diff == self.SCORE_DEUCE:
+            return self.DEUCE
+        else:
+            return "Invalid Score Difference"
 
     def _get_regular_score(self):
         p1_res = self.SCORE_MAPPING.get(self._p1_points, "Invalid")
@@ -38,7 +48,7 @@ class TennisGame2:
 
         if self._calculate_score_difference()==0:
             if self._p1_points == 3:
-                return "Deuce"
+                return self.DEUCE
             return f"{p1_res}-All"
         
         return f"{p1_res}-{p2_res}"
