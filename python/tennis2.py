de132487dfc06a3e94ed3471e38f19d37fd53652
georgialoukatou@gmail.com <georgialoukatou@gmail.com>
2024-10-15 00:24:22 +0200

    refactor(TennisGame2): add typing, Black and general formatting
    - Added type annotations to all functions and variables.
    - Applied Black code formatter for consistent code style.
    - Reordered functions.

--- python/tennis2.py
+++ python/tennis2.py
@@ -1,21 +1,24 @@
-class TennisGame2:
-    SCORE_MAPPING = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
-    DEUCE = "Deuce"
-    SCORE_ADVANTAGE_POSITIVE = 1
-    SCORE_ADVANTAGE_NEGATIVE = -1
-    SCORE_WIN_POSITIVE= 2
-    SCORE_WIN_NEGATIVE = -2
-    SCORE_DEUCE = 0
+from typing import Dict
 
-    def __init__(self, player1_name, player2_name):
+
+class TennisGame2:
+    SCORE_MAPPING: Dict[int, str] = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
+    DEUCE: str = "Deuce"
+    SCORE_ADVANTAGE_POSITIVE: int = 1
+    SCORE_ADVANTAGE_NEGATIVE: int = -1
+    SCORE_WIN_POSITIVE: int = 2
+    SCORE_WIN_NEGATIVE: int = -2
+    SCORE_DEUCE: int = 0
+
+    def __init__(self, player1_name: str, player2_name: str) -> None:
         if player1_name == player2_name:
             raise ValueError("Players have the same name")
-        self._player1_name = player1_name
-        self._player2_name = player2_name
-        self._p1_points = 0
-        self._p2_points = 0
+        self._player1_name: str = player1_name
+        self._player2_name: str = player2_name
+        self._p1_points: int = 0
+        self._p2_points: int = 0
 
-    def won_point(self, player_name):
+    def won_point(self, player_name: str) -> None:
         if player_name == self._player1_name:
             self._increment_p1_score()
         elif player_name == self._player2_name:
@@ -23,12 +26,19 @@ class TennisGame2:
         else:
             raise ValueError("Invalid player name")
 
-    def _calculate_score_difference(self):
-        score_diff = self._p1_points - self._p2_points
+    def score(self) -> str:
+        if max(self._p1_points, self._p2_points) >= 4:
+
+            return self._get_advantage_or_win_score()
+
+        return self._get_regular_score()
+
+    def _calculate_score_difference(self) -> int:
+        score_diff: int = self._p1_points - self._p2_points
         return score_diff
 
-    def _get_advantage_or_win_score(self):
-        score_diff = self._calculate_score_difference()
+    def _get_advantage_or_win_score(self) -> str:
+        score_diff: int = self._calculate_score_difference()
         if score_diff == self.SCORE_ADVANTAGE_POSITIVE:
             return f"Advantage {self._player1_name}"
         elif score_diff == self.SCORE_ADVANTAGE_NEGATIVE:
@@ -42,39 +52,31 @@ class TennisGame2:
         else:
             return "Invalid Score Difference"
 
-    def _get_regular_score(self):
-        p1_res = self.SCORE_MAPPING.get(self._p1_points, "Invalid")
-        p2_res = self.SCORE_MAPPING.get(self._p2_points, "Invalid")
+    def _increment_p1_score(self) -> None:
+        self._p1_points += 1
 
-        if self._calculate_score_difference()==0:
+    def _increment_p2_score(self) -> None:
+        self._p2_points += 1
+
+    def _get_regular_score(self) -> str:
+        p1_res: str = self.SCORE_MAPPING.get(self._p1_points, "Invalid")
+        p2_res: str = self.SCORE_MAPPING.get(self._p2_points, "Invalid")
+
+        if self._calculate_score_difference() == 0:
             if self._p1_points == 3:
                 return self.DEUCE
             return f"{p1_res}-All"
-        
+
         return f"{p1_res}-{p2_res}"
 
-    def score(self):
-        result = ""
-        if max(self._p1_points, self._p2_points) >= 4:
-            return self._get_advantage_or_win_score()
-
-        return self._get_regular_score()
-
-    def _increment_p1_score(self):
-        self._p1_points += 1
-
-    def _increment_p2_score(self):
-        self._p2_points += 1
-
-    def set_p1_score(self, number):
+    def set_p1_score(self, number: int) -> None:
         if number < 0:
             raise ValueError("Score cannot be negative.")
         for _ in range(number):
-            self.increment_p1_score()
+            self._increment_p1_score()
 
-    def set_p2_score(self, number):
+    def set_p2_score(self, number: int) -> None:
         if number < 0:
-           raise ValueError("Score cannot be negative.")
+            raise ValueError("Score cannot be negative.")
         for _ in range(number):
-            self.increment_p2_score()
-
+            self._increment_p2_score()
