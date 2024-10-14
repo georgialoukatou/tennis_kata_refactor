f9db05e0362f1ccf1159c4cf031383e04a5f8579
georgialoukatou@gmail.com <you@example.com>
2024-10-14 21:23:47 +0200

    refactor(TennisGame2): extract win and advantage conditions to new helper function
    - Moved win and advantage conditions from the 'score' method to a '_get_advantage_or_win_score' helper function.
    - Used score_diff for advantage conditions as well.
    - Added  Deuce condition to handle cases where both players have equal scores after the 4th round.

--- python/tennis2.py
+++ python/tennis2.py
@@ -15,20 +15,24 @@ class TennisGame2:
         else:
             raise ValueError("Invalid player name")
 
+
+    def _get_advantage_or_win_score(self):
+        score_diff = self.p1_points - self.p2_points
+        if score_diff >= 2:
+            return "Win for player1"
+        if score_diff <= -2:
+            return "Win for player2"
+        if score_diff == 1:
+            return "Advantage player1"
+        if score_diff == -1:
+            return "Advantage player2"
+        if score_diff == 0:
+            return "Deuce"
+
     def score(self):
         result = ""
-
-        score_diff = self.p1_points - self.p2_points
-        if self.p1_points >= 4 and score_diff >= 2:
-            return "Win for player1"
-        if self.p2_points >= 4 and score_diff <= -2:
-            return "Win for player2"
-
-        if self.p1_points > self.p2_points and self.p2_points >= 3:
-            return "Advantage player1"
-
-        if self.p2_points > self.p1_points and self.p1_points >= 3:
-            return "Advantage player2"
+        if max(self.p1_points, self.p2_points) >= 4:
+            return self._get_advantage_or_win_score()
 
         if self.p1_points == self.p2_points and self.p1_points < 3:
             result = self.SCORE_MAPPING.get(self.p1_points, "Invalid")   
